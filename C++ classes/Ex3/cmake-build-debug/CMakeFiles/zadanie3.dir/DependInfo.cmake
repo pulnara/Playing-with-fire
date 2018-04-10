0c5484d27a1c143b649d401c6fa6564e4f5e0c4c
pulnara <magnes.agnieszka@gmail.com>
2018-04-10 23:17:21 +0200

    Add some simple cpp code

---
+++ C++ classes/Ex3/cmake-build-debug/CMakeFiles/zadanie3.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aga/Dokumenty/studia/IV sem/C++/zadanie3/classes.cpp" "/home/aga/Dokumenty/studia/IV sem/C++/zadanie3/cmake-build-debug/CMakeFiles/zadanie3.dir/classes.cpp.o"
+  "/home/aga/Dokumenty/studia/IV sem/C++/zadanie3/main.cpp" "/home/aga/Dokumenty/studia/IV sem/C++/zadanie3/cmake-build-debug/CMakeFiles/zadanie3.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
